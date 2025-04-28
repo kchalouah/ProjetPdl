8ab0b7b88e1b4ec43ec50462ebe97f85c0af0443
kchalouah <100380797+kchalouah@users.noreply.github.com>
2025-04-28 12:46:03 +0100

    creattion de projet
    -first commit-

---
+++ Back/dossiermedicale/src/main/java/com/pdl/dossiermedicale/DossiermedicaleApplication.java
@@ -0,0 +1,13 @@
+package com.pdl.dossiermedicale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DossiermedicaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DossiermedicaleApplication.class, args);
+    }
+
+}
